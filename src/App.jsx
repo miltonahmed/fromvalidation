af929602e8f7ae8990a907ae770e8fe329b20800
Milton Ahmed <miltonahmed08@yahoo.com>
2025-07-13 00:12:06 +0400

    Environment Setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    
+    
+
+    <div className=' min-h-screen flex items-center justify-center bg-gray-400p-6'>
+     <h2 className="text-2xl font-bold mb-6 text-center text-indigo-600">
+          Signup Form
+        </h2>
+
+    </div>
+
+
+    </>
+    
+  )
+}
+
+export default App
